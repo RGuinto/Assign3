8029f45a8cd674b6fde20e87b2e91bca94bf1281
unknown <bondragie@gmail.com>
2015-03-10 11:24:17 -0800

    main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
-  $('#title').focus();
+    $('#title').focus();
     $('#text').autosize();
+
+    var food = ["BANANA","APPLE"];
+    var len = food.length;
+    for (var i=0;i<len;i++){
+
+        food.push(food[i].toLowerCase());
+
+    }
 });
