e8ddbf1751ce7e8298a9fb6b39c00c2aa5d4da3e
unknown <bondragie@gmail.com>
2015-03-23 01:28:56 -0800

    Changing to a new to-do list template

--- js/script.js
+++ js/script.js
@@ -1,96 +1,60 @@
-var dbName = 'todo_list_store_2';
 
-sklad.open(dbName, {
-  version: 2,
-  migration: {
-    '1': function (database) {
-      var objStore = database.createObjectStore('todos', {autoIncrement: true});
-      objStore.createIndex('description_search', 'description', {unique: true});
-    },
-    '2': function (database) {
-      database.deleteObjectStore('todos');
-      var objStore = database.createObjectStore('todos', {
-        autoIncrement: true,
-        keyPath: 'timestamp'
-      });
-      objStore.createIndex('description_search', 'description', {unique: true});
-      objStore.createIndex('done', 'done', {unique: false});
-    }
-  }
-}, function (err, conn) {
-  if (err) { throw err; }
-  $(function () {
-    var $description = $('#description');
-    var $add         = $('#add');
-    var $list        = $('#list');
-    var food = ["BANANA","APPLE"];
-    var len = food.length;
-    for (var i=0;i<len;i++){
-
-        food.push(food[i].toLowerCase());
-    }
-
-    var rist = document.getElementsByTagName("li");
-    for (each item in the list in ul){
-      for(var i=0;i<=food.length;i++){
-        if (description[i] == food[j]){
-          $li[i].attr('class', 'keyword');
-        }
-      }
-    }
-    
-    function updateRows(conn) {
-      conn
-        .get({
-          todos: {description: sklad.DESC, index: 'description_search'}
-        }, function (err, data) {
-          if (err) { return console.error(err); }
-          
-          $list.empty();
-          data.todos.forEach(function (todo) {
-            var $li = $(document.createElement('li'));
-            if (todo.value.done) {
-              $li.css({textDecoration: 'line-through'});
-            }
-            $li.text(todo.value.description);
-            console.log(todo);
-            $li.click(function () {
-              todo.value.done = true;
-              conn.upsert('todos', todo.value, function (err) {
-                if (err) { return console.error(); }
-                updateRows(conn);
-              })
-            });
-            $list.append($li);
-          });
+function TodoCtrl($scope) {
+  
+  $scope.todos = [
+    {text:'Learn AngularJS', done:false},         
+    {text: 'Build an app', done:false}
+  ];
+  
+  $scope.getTotalTodos = function () {
+    return $scope.todos.length;
+  };
+  
+  
+  $scope.addTodo = function () {
+    $scope.todos.push({text:$scope.formTodoText, done:false});
+    $scope.formTodoText = '';
+  };
+  
+    $scope.clearCompleted = function () {
+        $scope.todos = _.filter($scope.todos, function(todo){
+            return !todo.done;
         });
-    }
- 
-    updateRows(conn);
-    $add.click(function () {
-      if (!$description.val().trim()) {
-        return;
-      }
+    };
+}
 
-      for(var i=0;i<=food.length;i++){
-        if ($description.val().trim() == food[i]){
-          $li[i].attr('class', 'keyword');
-        }
-      }
+//     // Color coding part. Not yet working.
+//     var food = ["BANANA","APPLE"];
+//     var len = food.length;
+//     for (var i=0;i<len;i++){
 
-      conn.insert({
-        todos: [
-          {
-            timestamp: Date.now(),
-            description: $description.val().trim(),
-            done: false
-          }
-        ]
-      }, function (err, insertedKeys) {
-        if (err) { return console.error(err); }
-        $description.val('');
-        updateRows(conn);
-      })
-    });
-  });
-});
+//         food.push(food[i].toLowerCase());
+//     }
+
+//     $('#list').find("li").each(function (i){
+//       var description = $(this).text();
+//       for (var j=0; j<=food.length;j++){
+//         if (description == food[j]){
+//           $('#list').find('li')[i].addClass('keyword');
+//         }
+//       }
+//     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
